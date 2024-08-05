35d46038965b34252db9816ad9cd904ea83d895c
saurav0909 <sauravmali164189@gmail.com>
2024-08-05 13:56:57 +0530

    Add new feature

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("This is new feature");
         App obj=new App();
         obj.add(10,5);
     }
