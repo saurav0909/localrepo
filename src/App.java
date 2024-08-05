1c3939cb37f344b5794989a834fdb15677de0057
saurav0909 <sauravmali164189@gmail.com>
2024-08-05 13:27:16 +0530

    Add initial file

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+
+    public void add(int n1, int n2){
+        int sum=n1+n2;
+        System.out.println(sum);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        App obj=new App();
+        obj.add(10,5);
+    }
+}
